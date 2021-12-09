255106948d47df80d17dc58d05a49378a6354228
Ilya <ilyagonnov@gmail.com>
2021-12-10 00:39:39 +0400

    add enqury controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service';
-
-@Controller()
+import { Request } from 'express';
+const enquryList = [
+  {
+    name: 'ilya',
+    age: 12,
+    address: '5avenu',
+    done: false,
+  },
+  {
+    name: 'vlad',
+    age: 13,
+    address: '1avenu',
+    done: false,
+  },
+  {
+    name: 'sasha',
+    age: 0,
+    address: '53avenu',
+    done: false,
+  },
+];
+@Controller('enquries')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Header('Content-Type', 'application/json')
+  @Post()
+  findAll(@Req() request: Request): string {
+    return JSON.stringify(enquryList);
   }
 }
