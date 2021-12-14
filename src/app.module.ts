dda706d3d15ebacdd834a7fb03657e9235dd887f
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2021-12-14 23:41:40 +0400

    feature: findMany controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { PrismaService } from './prisma.service';
+import { UserController } from './users/user.controller';
+import { UserService } from './users/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [PrismaService],
+  controllers: [UserController],
+  providers: [PrismaService, UserService],
 })
 export class AppModule {}
